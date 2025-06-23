79d7e4c250e14efb224b479ad92a70c711ea5c86
Saurav Kapadiya <sauravk.vebuin@gmail.com>
2025-06-23 20:04:22 +0530

    Deleted Unnecessary files

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Add the path to the functions directory
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
--- tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from main import add
-
-
-def test_add_function():
-    assert add(2,3) == 5
-    assert add(0,0) == 0
-    assert add(5,5) == 10
